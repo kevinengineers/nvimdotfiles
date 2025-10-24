7ff483b3d42fb4b970dca0e6085da781f1d6f4f4
kevinengineers <kevinengineers@proton.me>
2025-10-23 21:33:24 -0700

    added several plugins

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,13 +4,13 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
      local builtin = require("telescope.builtin")
-     vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
-     vim.keymap.set('n', '<leader>gf', builtin.live_grep, {})
+     vim.keymap.set('n', '<leader>sf', builtin.find_files, {desc = "search for files"})
+     vim.keymap.set('n', '<leader>gf', builtin.live_grep, {desc = "grep file"})
     end
   },
   {
     "nvim-telescope/telescope-ui-select.nvim",
-    config = function() 
+    config = function()
       require('telescope').setup({
         extensions = {
           ["ui-select"] = {
